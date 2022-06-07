0611c7c89aeac74dbf5b1542fa7ee8d1ecfa42d1
drew08t <drew08@gmail.com>
2022-06-06 17:21:33 -0700

    GeomapPanel: add hideOpacity and apply to markers

--- public/app/plugins/panel/geomap/editor/layerEditor.tsx
+++ public/app/plugins/panel/geomap/editor/layerEditor.tsx
@@ -91,7 +91,7 @@ export function getLayerEditor(opts: LayerEditorOptions): NestedPanelOptions<Map
         handler.registerOptionsUI(builder);
       }
       if (!isEqual(opts.category, ['Base layer'])) {
-        if (layer.showOpacity) {
+        if (!layer.hideOpacity) {
           builder.addSliderInput({
             path: 'opacity',
             name: 'Opacity',
